bfa2313a02e10d561e4e2ada807c3d51336db97c
Ashwini <ashwini.basatti@gmail.com>
2021-08-12 15:47:17 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const fs = require('fs')
+
+const value = require('./data')
+
+const userData = value.data("Prograd",2020,"BE")
+const UserName = "Ashwini"
+const Password = "ash0818"
+
+const crypto = require('crypto')
+const password = crypto.createHmac('sha256', 'secret').update(Password).digest('hex');
+fs.writeFileSync('message.txt',
+`Prograd Details\n
+Name -> ${userData.name}
+Year -> ${userData.Year}
+Qualification -> ${userData.Qualification}
+UserName -> ${UserName}
+Password -> ${Password}`
+)
+
+
+
 
